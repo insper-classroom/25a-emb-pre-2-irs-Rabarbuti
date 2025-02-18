027e7430d42508390052d37da3f1570c9e2f62bd
raphaelbb1 <rabarbuti@gmail.com>
2025-02-18 15:37:20 -0300

    ex4

--- exe4/main.c
+++ exe4/main.c
@@ -8,28 +8,18 @@ const int BTN_PIN_G = 26;
 const int LED_PIN_R = 4;
 const int LED_PIN_G = 6;
 volatile int fall_red = 0;
-volatile int fall_green = 0;
-volatile int rise_red = 0;
 volatile int rise_green = 0;
 
 void btn_callback(uint gpio, uint32_t events) {
   if (events == 0x4) { // fall edge
     if (gpio == BTN_PIN_R){
-      rise_red = 0;
       fall_red = 1;
-    }else if (gpio == BTN_PIN_G){
-      rise_green = 0;
-      fall_green = 1;
-  }
+    }
   }
   if (events == 0x8) { // rise edge
-    if (gpio == BTN_PIN_R){
-      fall_red = 0;
-      rise_red = 1;
-    }else if (gpio == BTN_PIN_G){
-      fall_green = 0;
+    if (gpio == BTN_PIN_G){
       rise_green = 1;
-  }}
+    }}
 }
 
 
@@ -52,23 +42,15 @@ int main() {
     &btn_callback);
 
   // callback led g (nao usar _with_callback)
-  gpio_set_irq_enabled(BTN_PIN_G, GPIO_IRQ_EDGE_FALL, true);
+  gpio_set_irq_enabled(BTN_PIN_G, GPIO_IRQ_EDGE_RISE, true);
 
   while (true) {
-    if (fall_red ==1){
-      gpio_put(LED_PIN_R, 1);
+    if (fall_red){
+      gpio_put(LED_PIN_R, !gpio_get(LED_PIN_R));
       fall_red = 0;
     }
-    if (rise_red == 1){
-      gpio_put(LED_PIN_R, 0);
-      rise_red = 0;
-    }
-    if (fall_green ==1){
-      gpio_put(LED_PIN_G, 1);
-      fall_green = 0;
-    }
     if (rise_green == 1){
-      gpio_put(LED_PIN_G, 0);
+      gpio_put(LED_PIN_G, !gpio_get(LED_PIN_G));
       rise_green = 0;
   }
 }
